5a2adf83579366fd92ef9e9ded4a1e685598db47
unzipnav <unzip.nav@gmail.com>
2023-01-14 23:57:13 +0530

    Controller updated as RestController

--- controller/InterviewController.java
+++ controller/InterviewController.java
@@ -3,10 +3,14 @@ package com.scaler.assignment.controller;
 import com.scaler.assignment.models.Interview;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.http.HttpResponse;
 
-@Controller
+@RestController
 public class InterviewController {
 
     private InterviewService interviewService;
@@ -15,18 +19,20 @@ public class InterviewController {
         this.interviewService = interviewService;
     }
 
-    CreateInterviewRequestDto createInterviewRequestDto = new CreateInterviewRequestDto();
+//    CreateInterviewRequestDto createInterviewRequestDto = new CreateInterviewRequestDto();
 
+    @PostMapping
     public HttpResponse createInterview () {
         return interviewService.createInterview(.....);
     }
 
+    @PutMapping
     public HttpResponse updateInterview () {
         return interviewService.updateInterview(...);
     }
 
+    @DeleteMapping
     public HttpResponse deleteInterview () {
-                
 
     }
 }
