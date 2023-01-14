255ab61e575ecbc3d46b250b0eb31d3c8d62268a
unzipnav <unzip.nav@gmail.com>
2023-01-15 00:43:46 +0530

    added UserService for User

--- services/UserService.java
+++ services/UserService.java
@@ -1,2 +1,16 @@
-package com.scaler.assignment.services;public class UserService {
+package com.scaler.assignment.services;
+
+import com.scaler.assignment.models.User;
+import com.scaler.assignment.repository.UserRepository;
+
+public class UserService {
+    private UserRepository userRepository;
+
+    public UserService (UserRepository userRepository)  {
+        this.userRepository = userRepository;
+    }
+
+    public User addUser (User user) {
+        return null;
+    }
 }
